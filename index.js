773506935428608972c91ae5426febe431a6d60f
Rayquen <rayquen.zero@gmail.com>
2021-01-07 20:16:37 -0300

    implemento body-parser

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+
+const bodyParser = require('body-parser')
+
 const login = require('./routes/api/login.route.js')
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .use('/api/login', login.route )
+const app = express()
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.get('/', (req, res) => res.render('pages/index'))
+app.use( bodyParser.urlencoded( { extended: true }) )
+app.use( bodyParser.json() )
+app.use('/api/login', login.route )
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
