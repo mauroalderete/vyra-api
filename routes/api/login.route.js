7d04c9dca447d94a80f2d46bf6361c8aa52f3b4d
Rayquen <rayquen.zero@gmail.com>
2021-01-07 21:16:54 -0300

    construyo usuario de ejemplo e implemento un login simple

--- routes/api/login.route.js
+++ routes/api/login.route.js
@@ -1,15 +1,35 @@
 const express = require('express');
 const jwt = require('jsonwebtoken')
+const UserModel = require('../../models/user.model')
+
+const mauro = new UserModel('mauro', '123456', process.env.UID)
+const sergio = new UserModel('sergio', '123456', process.env.UID)
 
 let route = express();
 
 route.post('/', (req,res) => {
 
-    console.log('body: ', req.body)
+    const user = new UserModel( req.body )
 
-    res.json({
-        response: 'Ok'
-    })
+    if( user.equal( mauro ) || user.equal( sergio ) ){
+
+        console.log(Date.now())
+        const token = jwt.sign(
+            { user: user, timestamp: Date.now() },
+            process.env.TOKEN_SECRET,
+            {
+                expiresIn: parseInt(process.env.EXPIRE_JWT, 10) || 1440
+            }
+        )
+
+        res.json({
+            response: "Autenticación correcta",
+            token
+        });
+
+    } else {
+        res.status(400).send('Acceso invalido')
+    }
 });
 
 module.exports = { route };
