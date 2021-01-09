9b9af3cf846564256b80bada70dfca36a49bf200
Rayquen <rayquen.zero@gmail.com>
2021-01-09 14:41:55 -0300

    prueba de conexion simple con pg local

--- modules/stoc/stoc_marcas/routes/marcas.route.js
+++ modules/stoc/stoc_marcas/routes/marcas.route.js
@@ -1,17 +1,34 @@
 const express = require('express');
+const { Pool } = require('pg')
 
 let route = express();
 
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: process.env.NODE_ENV == 'production' ? {rejectUnauthorized: false} : ''
+})
+
+console.log('MARCAS::')
+console.log(pool)
+
 route.get('/', (req,res) => {
-    res.json({
-        marcas: 'lista de marcas'
-    })
 
     try{
-
+        pool.connect().then( (client) => {
+            client.query('SELECT * FROM STOC_MARCAS').then( (result) => {
+                res.status(200).send( result )
+                client.release()
+            }).catch( reason => {
+                console.error(reason);
+                res.status(409).send('[ERROR:Query] ' + reason)
+            } )
+        }).catch( reason => {
+            console.error(reason)
+            res.status(409).send('[ERROR:Connect] ' + reason)
+        })
     } catch(err){
         console.error(err);
-        res.status(409).send('Error: ' + err)
+        res.status(409).send('[ERROR:Undefined] ' + err)
     }
 })
 
