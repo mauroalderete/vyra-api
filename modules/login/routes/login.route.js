93812981de7da88d1e6ee7d253a997ae065549ba
Rayquen <rayquen.zero@gmail.com>
2021-01-16 18:45:41 -0300

    formatea la fecha

--- modules/login/routes/login.route.js
+++ modules/login/routes/login.route.js
@@ -36,4 +36,21 @@ route.post('/', (req,res) => {
     }
 });
 
+function formatTimestamp(date) {
+    var d = new Date(date)
+    month = '' + (d.getMonth() + 1)
+    day = '' + d.getDate()
+    year = d.getFullYear()
+    hour = '' + d.getHours()
+    minutes = '' + d.getMinutes()
+    seconds = '' + d.getSeconds()
+
+    if (month.length < 2) 
+        month = '0' + month;
+    if (day.length < 2) 
+        day = '0' + day;
+
+    return `${year}${month}${day} ${hour}:${minutes}:${seconds}`;
+}
+
 module.exports = { route };
